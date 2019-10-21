77cb3da75c6d7ec2b849b44d1b401641207a2ccc
Punit Naik <naik.punit44@gmail.com>
2019-10-21 17:52:48 +0530

    :fire: extracted out join files fn

--- src/Lib.hs
+++ src/Lib.hs
@@ -32,21 +32,25 @@ groupByAuthorId x = DMap.fromList
 justLookupArticles :: Map Int [ArtSpec.Article] -> Int -> [ArtSpec.Article]
 justLookupArticles m k = DMaybe.fromMaybe [] (DMap.lookup k m)
 
+joinAuthorsAndArticles :: [AuthSpec.Author] -> Map Int [ArtSpec.Article] -> [String] -> [AuthArtSpec.AuthorWithArticles]
+joinAuthorsAndArticles authors groupedArticles filters = do
+                                  DList.map (\x -> do
+                                    let articlez = (justLookupArticles groupedArticles (AuthSpec.id x))
+                                    let filteredArticles = DList.filter (\x -> do
+                                                                          (rating x) > (if (DList.elem "filter=ratingGreater3" filters) then 3 else 0))
+                                                            (DList.take (if (DList.elem "filter=first5" filters)
+                                                                          then 5
+                                                                          else (DList.length articlez))
+                                                              articlez)
+                                    AuthArtSpec.AuthorWithArticles
+                                      (AuthSpec.id x) (AuthSpec.name x) filteredArticles) authors
+
 joinFiles :: String -> String -> [String] -> IO ()
 joinFiles authors articles filters = do
   authorsFile <- BS.readFile (authors)
   articlesFile <- BS.readFile (articles)
   let authors = JU.parseAuthorsJSON authorsFile
   let groupedArticles = groupByAuthorId (JU.parseArticlesJSON articlesFile)
-  let authorsWithArticles = DList.map (\x -> do
-                                          let articlez = (justLookupArticles groupedArticles (AuthSpec.id x))
-                                          let filteredArticles = DList.filter (\x -> do
-                                                                                (rating x) > (if (DList.elem "filter=ratingGreater3" filters) then 3 else 0))
-                                                                  (DList.take (if (DList.elem "filter=first5" filters)
-                                                                                then 5
-                                                                                else (DList.length articlez))
-                                                                    articlez)
-                                          AuthArtSpec.AuthorWithArticles
-                                             (AuthSpec.id x) (AuthSpec.name x) filteredArticles) authors
+  let authorsWithArticles = joinAuthorsAndArticles authors groupedArticles filters
   let d = encodeToLazyText authorsWithArticles
-  I.writeFile "joined.json" d
+  I.writeFile "authors_with_articles.json" d
